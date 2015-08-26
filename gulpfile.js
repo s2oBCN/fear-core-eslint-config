f06e7a05cfa23ff2eaef62b40f4b4499d21f3933
Marton Hipszki <marton.hipszki@marks-and-spencer.com>
2015-08-26 15:40:56 +0100

    created basic helper gulp tasks (linting + running tests)

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var eslint = require('gulp-eslint');
+
+gulp.task('test', ['lint'], function() {
+    return gulp.src(['test/*.spec.js'], { read: false })
+        .pipe(mocha({
+            reporter: 'spec',
+            globals: {
+                expect: require('chai').expect
+            }
+    }));
+});
+
+gulp.task('lint', function () {
+    return gulp.src(['*-config.js', 'test/*.js'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failOnError());
+});
+
+gulp.task('watch', function() {
+    gulp.watch(['*-config.js', 'test/*.spec.js'], ['test']);
+});
+
