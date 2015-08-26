8d231587192e950fef5e2f58d9de5a74bc9d1461
Marton Hipszki <marton.hipszki@marks-and-spencer.com>
2015-08-26 16:17:31 +0100

    added browser specific config

---
+++ test/browser-config.spec.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var expect = require('chai').expect;
+var config = require('../browser-config');
+
+describe('browser specific config', function () {
+
+    it('should extend common config', function() {
+        expect(config.extends).to.equal('fear-core/common-config');
+    });
+
+    it('should define browser as the main environment', function() {
+        expect(config.env).to.include.keys('browser');
+    });
+
+    it('should expect Use Strict Directive inside functions', function() {
+        expect(config.rules.strict).to.contain('function');
+    });
+
+});
