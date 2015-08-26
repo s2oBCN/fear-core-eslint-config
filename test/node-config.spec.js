836fb7d247f5b419fd05788ceb55fd2b14a95950
Marton Hipszki <marton.hipszki@marks-and-spencer.com>
2015-08-26 16:18:35 +0100

    added node specific config

---
+++ test/node-config.spec.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var expect = require('chai').expect;
+var config = require('../node-config');
+
+describe('node specific config', function () {
+
+    it('should extend common config', function() {
+        expect(config.extends).to.equal('fear-core/common-config');
+    });
+
+    it('should define node as the main environment', function() {
+        expect(config.env).to.include.keys('node');
+    });
+
+    it('should expect global Use Strict Directive', function() {
+        expect(config.rules.strict).to.contain('global');
+    });
+
+});
