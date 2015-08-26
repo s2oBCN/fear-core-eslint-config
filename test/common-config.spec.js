d73fcfef1bdbfa2945feec63af701c91d253b448
Marton Hipszki <marton.hipszki@marks-and-spencer.com>
2015-08-26 16:19:03 +0100

    refactored common config spec

--- test/common-config.spec.js
+++ test/common-config.spec.js
@@ -1,20 +1,20 @@
 'use strict';
 
 var expect = require('chai').expect;
-var commonConfig = require('../common-config');
+var config = require('../common-config');
 
 describe('common config', function () {
 
     it('should extend eslint:recommended rules', function() {
-        expect(commonConfig.extends).to.equal('eslint:recommended');
+        expect(config.extends).to.equal('eslint:recommended');
     });
 
     it('should define EcmaScript 2015 / ES6 environment', function() {
-        expect(commonConfig.env).to.include.keys('es6');
+        expect(config.env).to.include.keys('es6');
     });
 
     it('should have default rules defined', function() {
-        expect(commonConfig.rules.indent).to.contain(2);
+        expect(config.rules.indent).to.contain(2);
     });
 
 });
